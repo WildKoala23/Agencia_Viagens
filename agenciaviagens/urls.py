e5d447cd4ebd466cdac67728cfebe2a6f2fc68ca
francisco5321 <frvitorino04fr@gmail.com>
2025-09-24 15:23:26 +0100

    url fix

--- agenciaviagens/urls.py
+++ agenciaviagens/urls.py
@@ -16,11 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 from minha_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', views.listar_clientes, name='listar_clientes'),
     path('inserir_cliente/', views.inserir_cliente, name='inserir_cliente'),
+
+    # redireciona "/" para "/clientes/"
+    path('', RedirectView.as_view(url='/clientes/', permanent=False)),
 ]
 
