f04a51baf2797f31f81c50afc30987d4e31001cd
Guilherme Pedrinho <pv25215@alunos.estgv.ipv.pt>
2025-10-25 19:00:48 +0000

    Added routes for dev

--- pacotes/urls.py
+++ pacotes/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("destinos", views.destinos, name="destinos"),
     path("eliminar/<int:destino_id>/", views.eliminar_destino, name="eliminar_destino"),
+    path("pacotes", views.pacotes, name="pacotes"),
+    path("voos", views.voos, name="voos"),
+    path("eliminar/<int:voo_id>/", views.eliminar_voo, name="eliminar_voo"),
 ]
