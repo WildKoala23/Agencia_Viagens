05fa2bb1649ecf591efb325359da02af73b80c83
Rafael-R-08 <raffael.roddrigues08@gmail.com>
2025-10-28 12:50:44 +0000

    Add new routes for fatura management

--- pagamentos/urls.py
+++ pagamentos/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
   path('pagamentos', views.pagamentos, name='pagamentos'),
   path('faturas', views.faturas, name='faturas'),
+  path('faturas/criar-completa/', views.criar_fatura_completa, name='criar_fatura_completa'),
+  path('faturas/editar/<int:fatura_id>/', views.editar_fatura, name='editar_fatura'),
+  path('faturas/eliminar/<int:fatura_id>/', views.eliminar_fatura, name='eliminar_fatura'),
+  path('faturas/atualizar-total/<int:fatura_id>/', views.atualizar_total_fatura, name='atualizar_total_fatura'),
+  path('faturas/<int:fatura_id>/adicionar-linha/', views.adicionar_linha_fatura, name='adicionar_linha_fatura'),
 ]
