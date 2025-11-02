23cd613c395663b88656fb4cacd2dcd528715c5b
Guilherme Pedrinho <pv25215@alunos.estgv.ipv.pt>
2025-11-02 12:34:13 +0000

    Change reservas page

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.user, name='user'),
-    path('compras/', views.comprasUser, name='comprasUser'),
-    path('insert/', views.clientes, name='clientes'),
+    path('compras', views.comprasUser, name='comprasUser'),
+    path('insert', views.clientes, name='clientes'),
     path('eliminar/<int:cliente_id>/', views.eliminar_cliente, name='eliminar_cliente'),
 ]
